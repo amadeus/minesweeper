5287920962e9dab7dfc07fbc3785988c2c476086
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2025-02-21 01:02:13 -0800

    Add proper eslintrc config file

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ['src/**/*.{js,mjs,cjs,ts,jsx,tsx}']},
+  {languageOptions: {globals: globals.browser}},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {rules: {'react/react-in-jsx-scope': 'off'}, settings: {react: {version: 'detect'}}},
+  {ignores: ['node_modules/', 'dist/']},
+];
